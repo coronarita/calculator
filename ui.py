e8bce9fb5a26f5745530c29f11e41386882f1620
developer1 <dev1@email.com>
2024-05-07 03:08:41 +0900

    Modify ui.py to add le1, le2 and cb widgets

--- ui.py
+++ ui.py
@@ -10,9 +10,12 @@ from PyQt5.QtWidgets import (
     QMessageBox,
     QPlainTextEdit,
     QHBoxLayout,
+    QLineEdit,
+    QComboBox,
 )
 
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore
 
 
 class View(QWidget):
@@ -28,6 +31,20 @@ class View(QWidget):
         self.btn1 = QPushButton("Message", self)
         self.btn2 = QPushButton("Clear", self)
 
+        self.le1 = QLineEdit("0", self)  # 라인 에디트1 추가
+        self.le1.setAlignment(QtCore.Qt.AlignRight)
+
+        self.cb = QComboBox(self)
+        self.cb.addItems(["+", "-", "*", "/"])
+
+        self.le2 = QLineEdit("0", self)  # 라인 에디트2 추가
+        self.le2.setAlignment(QtCore.Qt.AlignRight)
+
+        hbox_formular = QHBoxLayout()
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
+
         hbox = QHBoxLayout()
         hbox.addStretch(1)  # 공백
         hbox.addWidget(self.btn1)
@@ -35,6 +52,7 @@ class View(QWidget):
 
         vbox = QVBoxLayout()  # 수직 레이아웃
         vbox.addWidget(self.te1)
+        vbox.addLayout(hbox_formular)
         vbox.addLayout(hbox)
         vbox.addStretch(1)  # 빈 공간
 
